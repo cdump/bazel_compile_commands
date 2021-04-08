2b7895b25bf96152a96970fe0a0f2f4b0ef6bca8
Maxim Andreev <andreevmaxim@gmail.com>
2021-04-08 10:26:19 +0300

    improve hack for includes from workspace

--- gen_compile_commands.sh
+++ gen_compile_commands.sh
@@ -6,10 +6,11 @@ set -e
 JSON_OUT="$(pwd)/compile_commands.json"
 BAZEL_ROOT="$(bazel info execution_root 2>/dev/null)"
 WORKSPACE="$(bazel info workspace 2>/dev/null)"
+OUTPUT_BASE="$(bazel info output_base 2>/dev/null)"
 echo "Generating compile_commands.json..."
 find "$BAZEL_ROOT" -name '*.compile_command.json' -print0 | while read -r -d '' fname; do
     # dirty hack: what if we have 'external' dir in workspace?
-    sed -e "s|@BAZEL_ROOT@|$BAZEL_ROOT|g" < "$fname" | perl -pe "s#-isystem (?!bazel-out|external)#-isystem $WORKSPACE/#g" >> $JSON_OUT
+    perl -pe 'if (/"file":"external/){$p="'$OUTPUT_BASE'";}else{$p="'$WORKSPACE'";}; s/\@BAZEL_ROOT\@/$p/; s#-i(system|quote) external/#-i$1 '$OUTPUT_BASE'/external/#g' < "$fname" >> $JSON_OUT
     echo "" >> $JSON_OUT
 done
 
